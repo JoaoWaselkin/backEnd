036a1f0686286987d19da1de42b973811be4b353
Cx1que <caiquedev.estudos@gmail.com>
2024-12-01 18:24:50 -0300

    adiciona post e arquivo dotenv

--- src/controllers/contactController.ts
+++ src/controllers/contactController.ts
@@ -12,3 +12,15 @@ export const getContacts = async (req: Request, res: Response): Promise<void> =>
     res.status(500).json({ message: 'Erro ao buscar contatos' });
   }
 };
+
+export const postContact = async (req: Request, res: Response): Promise<void> => {
+  const { name, email, image, telefone } = req.body;
+
+  try {
+    const newContact = await contactRepo.addContact(name, email, image, telefone);
+    res.status(201).json(newContact);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Erro ao criar o contato' });
+  }
+};
--- src/repositories/contactRepository.ts
+++ src/repositories/contactRepository.ts
@@ -1,15 +1,24 @@
 import { Pool } from "pg";
-//import pool from "../config/database";
+import pool from "../config/database";
 import { Contact } from "../models/contactModel";
 
 export class ContactRepository {
   private pool: Pool;
 
-  constructor() {
-    this.pool = pool;
-  }
+constructor() {
+  this.pool = pool;
+}
 
-  async getAllContacts(): Promise<Contact[]> {
-    const { rows } = await this.pool.query("SELECT * FROM contactos");
-    return rows;
-  }
+async getAllContacts(): Promise<Contact[]> {
+  const { rows } = await this.pool.query("SELECT * FROM contactos");
+  return rows;
+}
+
+async addContact(name: string, email: string, image: string, telefone: string): Promise<Contact> {
+  const queryText =
+    "INSERT INTO contactos(name, email, image, telefone) VALUES($1, $2, $3, $4) RETURNING *";
+  const { rows } = await this.pool.query(queryText, [name, email, image, telefone]);
+  return rows[0];
+}
+
+}
